86f4302058b1bce3cc1120ede67dd5646194e8eb
jv.lee@foxmail.com <jv.lee@foxmail.com>
2020-01-23 18:40:29 +0800

    update code

--- test_fultter/lib/widget/scaffold/home.dart
+++ test_fultter/lib/widget/scaffold/home.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  PageController _pageController = PageController(initialPage: 0);
+  List tabs = ["新闻", "历史", "图片"];
+  var currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+    _tabController.addListener(() {
+      _pageController.jumpToPage(_tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text(
-          'Home',
-          style: TextStyle(fontSize: 30),
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Colors.blue,
+          height: 38,
+          child: TabBar(
+            controller: _tabController,
+            tabs: tabs
+                .map((e) => Tab(
+                      text: e,
+                    ))
+                .toList(),
+            indicatorColor: Colors.red,
+            labelColor: Colors.red,
+            unselectedLabelColor: Colors.black,
+          ),
         ),
-      ),
+        Expanded(
+          child: PageView.builder(
+            itemCount: tabs.length,
+            controller: _pageController,
+            itemBuilder: (context, index) {
+              return Text(tabs[index]);
+            },
+            onPageChanged: (index) {
+              _tabController.animateTo(index);
+            },
+          ),
+        )
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
   }
 }
